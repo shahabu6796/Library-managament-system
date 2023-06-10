f9f0890890c68df503404108e1562ab79cf96ac2
shahabu <mdshahabu6796@gmail.com>
2023-06-10 10:05:49 +0530

    project structure and database setup

---
+++ Library-managament-system/src/main/java/com/library/com/Library/managament/system/LibraryManagamentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.library.com.Library.managament.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagamentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManagamentSystemApplication.class, args);
+	}
+
+}
---
+++ Library-managament-system/src/test/java/com/library/com/Library/managament/system/LibraryManagamentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.com.Library.managament.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagamentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
