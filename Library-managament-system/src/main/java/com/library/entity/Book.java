56abffb2b8505f6d9ba499988a3df18dc283ea89
shahabu <mdshahabu6796@gmail.com>
2023-06-11 19:07:33 +0530

    unused imports optimized

--- Library-managament-system/src/main/java/com/library/entity/Book.java
+++ Library-managament-system/src/main/java/com/library/entity/Book.java
@@ -1,6 +1,9 @@
 package com.library.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,7 +18,7 @@ public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bookId = 0L;
-    private String bookName ="";
-    private Double bookPrice =0.0;
-    private String author ="";
+    private String bookName = "";
+    private Double bookPrice = 0.0;
+    private String author = "";
 }
--- Library-managament-system/src/main/java/com/library/entity/Cart.java
+++ Library-managament-system/src/main/java/com/library/entity/Cart.java
@@ -1,13 +1,14 @@
 package com.library.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -15,12 +16,12 @@ import java.util.List;
 @Data
 public class Cart {
     @Id
-    @SequenceGenerator(name = "cartId",sequenceName = "cartId")
+    @SequenceGenerator(name = "cartId", sequenceName = "cartId")
     @GeneratedValue(generator = "cartId")
     private long cartId = 0L;
     private LocalDateTime allotDate;
     private LocalDateTime submitDate;
     private Double lateFine = 0.0;
-    private String issueName ;
-    private Long bookId ;
+    private String issueName;
+    private Long bookId;
 }
